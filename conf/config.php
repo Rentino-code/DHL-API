1a250ed5941d356c0a5508b5a832a7f81c1bdf3b
Rentino Rentino <rentino744@gmail.com>
2024-11-02 23:03:52 +0300

    Update the changes

--- conf/config.php
+++ conf/config.php
@@ -1,40 +1,11 @@
 <?php
-/**
- * Default Sample Configuration file
- *
- * Copy and customize this file with your own settings
- *
- * You may create custom configuration file based on the following environment variable : APPLICATION_ENVIRONMENT.
- * The naming to respect is the following : 
- *
- *  conf/config-{APPLICATION_ENVIRONMENT}.php
- *
- *  e.g. conf/config-production.php | conf/config-staging.php | conf/config-commondev.php
- *
- * @project DHL
- */
-return array(
-    // AutoloadManager options
-    'autoloader' => array(
-
-        // Only scan once when a class is not found in the class map (this should be set to SCAN_NONE on production environment
-        'scanOptions' => autoloadManager::SCAN_ONCE,
-
-        // complete path to autoload file that contains the class map 
-        'dir' => sys_get_temp_dir() . '/dhl-api-autoload.php',
-    ),
-
-    // DHL related settings    
-    'dhl' => array(
-        // ID to use to connect to DHL
-        'id' => 'rentino744@gmail.com',
-
-        // Password to use to connect to DHL
-        'pass' => 'KauAc/V/@Wj/5p3',
-
-        // Shipper, Billing and Duty Account numbers
-        'shipperAccountNumber' => 'YOUR_NUMBER',
-        'billingAccountNumber' => 'YOUR_NUMBER',
-        'dutyAccountNumber' => 'YOUR_NUMBER',
-    ),
-);
+// Configuration file for DHL API settings
+return [
+    'dhl' => [
+        'id' => 'rentino744@gmail.com',  // Replace with your DHL Site ID
+        'pass' => 'KauAc/V/@Wj/5p3',     // Replace with your DHL Password
+        'shipperAccountNumber' => 'Your_Shipper_Account_Number',
+        'billingAccountNumber' => 'Your_Billing_Account_Number',
+        'dutyAccountNumber' => 'Your_Duty_Account_Number',
+    ],
+];
